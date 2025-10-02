ca50ef1d716c7c42fdabce18276a9e30303d9d7d
amirnk81 <amirhossein.naimaei81@gmail.com>
2025-10-02 18:49:30 +0330

    fix requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory, session , send_file
+from flask import Flask, render_template, request, redirect, url_for, flash, session 
 from datetime import datetime
 from flask_mail import Mail, Message
 
