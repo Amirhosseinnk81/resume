accc5f850c8a0a55073add44d8e4425b450c8cc8
Amirhosseinnk81 <amirhossein.naimaei81@gmail.com>
2026-08-07 16:18:07 +0330

    complete project in admin and site

--- repositories/project_repository.py
+++ repositories/project_repository.py
@@ -1,16 +1,9 @@
 import json
 import os
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-BASE_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..")
-)
-
-PROJECTS_FILE = os.path.join(
-    BASE_DIR,
-    "data",
-    "projects.json"
-)
+PROJECTS_FILE = os.path.join(BASE_DIR, "data", "projects.json")
 
 
 def get_projects():
@@ -18,11 +11,7 @@ def get_projects():
     if not os.path.exists(PROJECTS_FILE):
         return []
 
-    with open(
-        PROJECTS_FILE,
-        "r",
-        encoding="utf-8"
-    ) as file:
+    with open(PROJECTS_FILE, "r", encoding="utf-8") as file:
 
         return json.load(file)
 
@@ -38,15 +27,13 @@ def get_project_by_id(project_id):
 
     return None
 
+
 def create_project(project):
 
     projects = get_projects()
 
     if projects:
-        new_id = max(
-            project.get("id", 0)
-            for project in projects
-        ) + 1
+        new_id = max(project.get("id", 0) for project in projects) + 1
     else:
         new_id = 1
 
@@ -54,20 +41,12 @@ def create_project(project):
 
     projects.append(project)
 
-    with open(
-        PROJECTS_FILE,
-        "w",
-        encoding="utf-8"
-    ) as file:
+    with open(PROJECTS_FILE, "w", encoding="utf-8") as file:
 
-        json.dump(
-            projects,
-            file,
-            ensure_ascii=False,
-            indent=4
-        )
+        json.dump(projects, file, ensure_ascii=False, indent=4)
 
     return project
+
 
 def update_project(project_id, updated_data):
 
@@ -81,24 +60,17 @@ def update_project(project_id, updated_data):
 
             projects[index] = updated_data
 
-            with open(
-                PROJECTS_FILE,
-                "w",
-                encoding="utf-8"
-            ) as file:
+            with open(PROJECTS_FILE, "w", encoding="utf-8") as file:
 
-                json.dump(
-                    projects,
-                    file,
-                    ensure_ascii=False,
-                    indent=4
-                )
+                json.dump(projects, file, ensure_ascii=False, indent=4)
 
             return updated_data
 
     return None
 
+
 def delete_project(project_id):
+
     projects = get_projects()
 
     for index, project in enumerate(projects):
@@ -107,7 +79,9 @@ def delete_project(project_id):
 
             deleted_project = projects.pop(index)
 
-            # save_projects(projects)
+            with open(PROJECTS_FILE, "w", encoding="utf-8") as file:
+
+                json.dump(projects, file, ensure_ascii=False, indent=4)
 
             return deleted_project
 
